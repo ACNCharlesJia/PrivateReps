b89c2d8ba62b6a076a5f703d4f080958291e37ff
ACN_CharlesJia <68106633+ACNCharlesJia@users.noreply.github.com>
2021-04-21 14:56:09 +0800

    update deploy.sh

---
+++ tools/deploy.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Deploy the content of _site to 'origin/<BRANCH_NAME>'
+
+set -eu
+
+BRANCH_NAME="SNX"
+_no_branch=false
+
+init() {
+  if [[ -z ${GITHUB_ACTION+x} ]]; then
+    echo "ERROR: This script is not allowed to run outside of GitHub Action."
+    exit -1
+  fi
+
+  if [[ -z $(git branch -av | grep "$BRANCH_NAME") ]]; then
+    _no_branch=true
+    git branch "$BRANCH_NAME"
+  fi
+}
+
+execute() {
+	java -version
+	java -jar ./tools/SnxTools-jar-with-dependencies.jar
+}
+
+deploy() {
+  git config --global user.name "GitHub Actions"
+  git config --global user.email "github-actions[bot]@users.noreply.github.com"
+
+  git update-ref -d HEAD
+  git add -A
+  git commit -m "[Automation] SNX update No.${GITHUB_RUN_NUMBER}"
+
+  if $_no_branch; then
+    git push -u origin "$BRANCH_NAME"
+  else
+    git push -f
+  fi
+}
+
+main() {
+  init
+  execute
+  deploy
+}
+
+main
